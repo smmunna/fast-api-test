ac1c0666736412a2981a6a6446d7c90a970f879a
smmunna <minhazulabedinmunna@gmail.com>
2025-05-12 15:07:55 +0600

    added

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+   return {"message": "Hello World"}
+
+@app.get("/about")
+async def about():
+    return {"message":"Welcome to about page"}
+
+@app.get("/home/{name}")
+async def home(name: str = Path(..., min_length=3)):
+    return {"name": name}
